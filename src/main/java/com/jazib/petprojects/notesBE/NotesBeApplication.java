52a2c876aa59bf1dcdba8666fba841f97c7eca2e
jazibhassan <jazibulhassan95@gmail.com>
2025-11-23 15:17:05 +0000

    First Commit. Used https://start.spring.io/ to generate bare bones spring project

---
+++ src/main/java/com/jazib/petprojects/notesBE/NotesBeApplication.java
@@ -0,0 +1,13 @@
+package com.jazib.petprojects.notesBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jazib/petprojects/notesBE/NotesBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jazib.petprojects.notesBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
